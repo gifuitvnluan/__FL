a0abdfa14dccc9d73d956127d1423b45c86a3c67
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2019-09-26 17:11:11 +0700

    luan 20190926

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
 	// $('.fancybox').fancybox();
 
+	// Page middlemen
 	$("#sec_middlemen").height($(document).height() - $(".ft_middlemen").height());
 
 	$("#sec_middlemen>a").hover(
@@ -15,12 +16,30 @@ $(document).ready(function () {
 			$("#sec_middlemen>a").removeClass("a_nohover");
 		}
 	);
+	
+	// End Page middlemen
 
+	// Page restaurant
 	$('.owl-carousel.res_slide').owlCarousel({
 		loop:true,
 		margin:10,
 		nav:true,
 		items:1,
-	})
+		dots:false,
+		nav:false,
+		autoplay:true,
+		autoplayTimeout:5000,
+		autoplaySpeed:1000,
+		animateOut: 'fadeOut'
+	});
+	$('.owl-carousel.res_slide .owl-stage .item').height($(window).height());
+	setInterval(function(){ 
+		$('.owl-carousel.res_slide .owl-stage .item').height($(window).height());
+	 }, 3000);
+	//  $('.owl-carousel.res_slide .owl-stage .item').parallax("50%", 0.05);
 	
+	// End page restaurant
+	$(".sp_hamber").click(function() {
+		$(".hd_all .hd_fix .hd_menu").toggleClass("active");
+	});
 });
