6ea90ff88005b2e5d805151fd5f9efbd9ba57d28
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2019-09-26 10:38:22 +0700

    20190926 1030

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,19 @@
-$(document).ready(function() {
-	$('a#page-top').click(function() {
-		$('body,html').animate({scrollTop: 0}, 500);
-  			 return false;
-		});
-	 var topBtn = $('a#page-top');
-	 topBtn.hide();
-	 $(window).scroll(function () {
-	  if ($(this).scrollTop() > 150) {
-	   topBtn.fadeIn();
-	  } else {
-	   topBtn.fadeOut();
-	  }
-	 });
-	 
-	 $("#owlslider").owlCarousel({
-			  autoPlay : true,
-			  navigation : false, // Show next and prev buttons
-			  pagination:false,
-			  paginationNumbers:false,
-			  slideSpeed : 1000,
-			  paginationSpeed : 700,
-			  singleItem: true,
-			   items: 1
-		 }); // owl slide
+$(document).ready(function () {
 
-		 $('.icon-menu').click(function()
-		{
-			$('.wrap-nav').stop().slideToggle();
-		});
-		
-		
-		$('.fancybox').fancybox();
+	// $('.fancybox').fancybox();
 
+	$("#sec_middlemen").height($(document).height() - $(".ft_middlemen").height());
+
+	$("#sec_middlemen>a").hover(
+		function () {
+			$("#sec_middlemen>a").addClass("a_nohover");
+			$(this).removeClass("a_nohover");
+			$(this).addClass("a_hover");
+		},
+		function () {
+			$(this).removeClass("a_hover");
+			$("#sec_middlemen>a").removeClass("a_nohover");
+		}
+	);
 	
 });
-
