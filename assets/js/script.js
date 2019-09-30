6e1547ccfd6b7ab3a7e489a62fd32e922ab513ee
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2019-09-30 10:25:59 +0700

    coffee

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,62 +16,67 @@ $(document).ready(function () {
 			$("#sec_middlemen>a").removeClass("a_nohover");
 		}
 	);
-	
+
 	// End Page middlemen
 
 	// Page restaurant
-	$('.owl-carousel.res_slide').owlCarousel({
-		loop:true,
-		margin:10,
-		nav:true,
-		items:1,
-		dots:false,
-		nav:false,
-		autoplay:true,
-		autoplayTimeout:5000,
-		autoplaySpeed:1000,
-		animateOut: 'fadeOut'
-	});
-	$('.owl-carousel.res_slide .owl-stage .item').height($(window).height());
-	setInterval(function(){ 
-		$('.owl-carousel.res_slide .owl-stage .item').height($(window).height());
-	 }, 3000);
-	//  $('.owl-carousel.res_slide .owl-stage .item').parallax("50%", 0.05);
-
-	
 	var topBtn = $('.backtop_res'); // khai báo 1 biến topBtn bằng nút có id "back-to-top"
-    topBtn.hide(); // mặc định cho nút ẩn đi
-    $(function() {
-        $(window).scroll(function() { // sự kiện lăn chuột
-            if ($(this).scrollTop() > 100) { // nếu khoảng cách với top lớn hơn 100px
-                topBtn.fadeIn(); // thì hiện nút "back-to-top" lên
-            } else {
-                topBtn.fadeOut(); // ngược lại ẩn nút đi
-            }
-        });
-        topBtn.click(function() { // bắt sự kiện khi click vào nút
-            $('body,html').animate({
-                scrollTop: 0
-            }, 1000); // thời gian di chuyển về đầu trang
-            return false;
-        });
+	topBtn.hide(); // mặc định cho nút ẩn đi
+	$(function () {
+		$(window).scroll(function () { // sự kiện lăn chuột
+			if ($(this).scrollTop() > 100) { // nếu khoảng cách với top lớn hơn 100px
+				topBtn.fadeIn(); // thì hiện nút "back-to-top" lên
+			} else {
+				topBtn.fadeOut(); // ngược lại ẩn nút đi
+			}
+		});
+		topBtn.click(function () { // bắt sự kiện khi click vào nút
+			$('body,html').animate({
+				scrollTop: 0
+			}, 1000); // thời gian di chuyển về đầu trang
+			return false;
+		});
 	});
 	// back top
-	
-	$(".sp_hamber").click(function() {
+
+	$(".sp_hamber").click(function () {
 		$(".hd_all .hd_fix .hd_menu").toggleClass("active");
 	});
 	// menu sp
 
-	$(window).scroll(function() { 
-		if ($(this).scrollTop() > 130) { 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 130) {
 			$(".hd_all").addClass("scroll_bg");
 		} else {
 			$(".hd_all").removeClass("scroll_bg");
 		}
 	});
-	
+
 	// End page restaurant
+
+	// Page Coffee
+
+	$(window).scroll(function () {
+		var kq = "khong";
+		$('.testsec').each(function (i) {
+			heightdivtotop = $(this).offset().top;
+			heightdivtotopEnd = $(this).offset().top + $(this).height();
+			var scroll = $(window).scrollTop();
+
+			if (scroll >= heightdivtotop-200 && scroll <= heightdivtotopEnd) {
+				kq = i+1;
+				// $("#gnav li a").html("<span>"+scroll + " - "+heightdivtotop+" - "+heightdivtotopEnd+"</span>"+"<span>"+scroll + " - "+heightdivtotop+" - "+heightdivtotopEnd+"</span>");
+				$(".page-nav li").removeClass("active");
+				$(".page-nav li:nth-of-type("+kq+")").addClass("active");
+				// $("#gnav li:nth-child("+kq+") a, .page-nav li:nth-of-type("+kq+")").addClass("active");
+
+			}
+		});
+		$(".result").html(kq);
+		
+	});
+	
+	// End page Coffee
 
 	$('.hd_all .hd_fix .hd_menu ul li a, #res_slide .slide_text .slide_scroll').click(function () {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -89,4 +94,22 @@ $(document).ready(function () {
 			}
 		}
 	});
+
+	$(".ib_form select, .ib_form input").change(function () {
+		$(this).parent().find(".txt_value").text($(this).val());
+	});
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		items: 1,
+		dots: false,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplaySpeed: 1000,
+		animateOut: 'fadeOut'
+	});
+	//  $('.owl-carousel.res_slide .owl-stage .item').parallax("50%", 0.05);
 });
